a25bafbfa95ce4c3298314453547dcb1c899211f
afy861345 <afy861345@yahoo.com>
2025-01-23 11:02:02 +0300

    change

--- clinic/settings.py
+++ clinic/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
+# import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASE_URL="postgresql://postgres:ozlnlVBKDokrIMQxbukETsuwQHwLXKHp@junction.proxy.rlwy.net:43925/railway"
+# DATABASE_URL="postgresql://postgres:ozlnlVBKDokrIMQxbukETsuwQHwLXKHp@junction.proxy.rlwy.net:43925/railway"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -74,21 +74,26 @@ WSGI_APPLICATION = 'clinic.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':"railway",
-        'USER':"postgres",
-        "PASSWORD":"ozlnlVBKDokrIMQxbukETsuwQHwLXKHp",
-        'HOST':"",
-        "PORT":5432
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES={
-    "default":dj_database_url.config(default=DATABASE_URL,conn_max_age=1800)
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME':"railway",
+#         'USER':"postgres",
+#         "PASSWORD":"ozlnlVBKDokrIMQxbukETsuwQHwLXKHp",
+#         'HOST':"",
+#         "PORT":5432
+
+#     }
+# }
+# DATABASES={
+#     "default":dj_database_url.config(default=DATABASE_URL,conn_max_age=1800)
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -125,8 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'clinic/static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
